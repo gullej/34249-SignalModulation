fcff576263d9529e33f8cd32db13ca0f07f482c5
Wombatfreak <madsptak@gmail.com>
2023-06-15 11:51:18 +0200

    Updated TLM_tb

--- tb/tx/TLM_tb.vhd
+++ tb/tx/TLM_tb.vhd
@@ -36,12 +36,12 @@ architecture testbench of TLM_tb is
   signal pam_map_data   :  std_logic_vector(0 DOWNTO 0);
   signal pam_map_valid  :  std_logic;
 
-  signal fifo_data_in   :  std_logic_vector(DATA_WIDTH-1 DOWNTO 0);
-  signal fifo_data_out  :  std_logic_vector(DATA_WIDTH*8-1 DOWNTO 0);
-  signal fifo_read      :  std_logic;
-  signal fifo_write     :  std_logic;
-  signal fifo_full      :  std_logic;
-  signal fifo_empty     :  std_logic;
+  signal clk_sync_data_in   :  std_logic_vector(DATA_WIDTH-1 DOWNTO 0);
+  signal clk_sync_data_out  :  std_logic_vector(DATA_WIDTH*8-1 DOWNTO 0);
+  signal clk_sync_read      :  std_logic;
+  signal clk_sync_write     :  std_logic;
+  signal clk_sync_full      :  std_logic;
+  signal clk_sync_empty     :  std_logic;
 
   signal pulse_shaper_valid_in   :  std_logic;
   signal pulse_shaper_valid_out  :  std_logic;
@@ -50,7 +50,7 @@ architecture testbench of TLM_tb is
   signal match_filter_data_out   :  std_logic_vector(27 DOWNTO 0);
   signal match_filter_valid_out  :  std_logic;
 
-  signal clk_recovery_data_out   :  std_logic_vector(27 DOWNTO 0);
+  signal clk_recovery_data_out   :  std_logic_vector(3 DOWNTO 0);
   signal clk_recovery_wr_out     :  std_logic;
   signal clk_recovery_valid_out  :  std_logic;
 
@@ -61,6 +61,7 @@ architecture testbench of TLM_tb is
 --         Transaction Interface Declaration             --
 --vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv--
 
+  -- Graycode VC interfaces
   signal stream_tx_rec, stream_rx_rec : StreamRecType (
     DataToModel(0 DOWNTO 0),
     DataFromModel(DATA_WIDTH - 1 DOWNTO 0),
@@ -68,6 +69,7 @@ architecture testbench of TLM_tb is
     ParamFromModel(16 downto 0)
   );
 
+  -- Clock Synchroniser Transmitter interface
   signal  sync_tx_rec : StreamRecType (
     DataToModel(DATA_WIDTH - 1 DOWNTO 0),
     DataFromModel(DATA_WIDTH - 1 DOWNTO 0),
@@ -75,6 +77,7 @@ architecture testbench of TLM_tb is
     ParamFromModel(16 downto 0)
   );
 
+  -- Clock Synchroniser Receiver interface
   signal  sync_rx_rec : StreamRecType (
     DataToModel(8 * DATA_WIDTH - 1 DOWNTO 0),
     DataFromModel(8 * DATA_WIDTH - 1 DOWNTO 0),
@@ -82,6 +85,7 @@ architecture testbench of TLM_tb is
     ParamFromModel(16 downto 0)
   );
 
+  -- Pulse Shaper Tx/Rx interfaces
   signal  pulse_tx_rec, pulse_rx_rec : StreamRecType (
     DataToModel(8 * DATA_WIDTH - 1 DOWNTO 0),
     DataFromModel(13 DOWNTO 0),
@@ -89,31 +93,25 @@ architecture testbench of TLM_tb is
     ParamFromModel(16 downto 0)
   );
 
-
 -----------------------------------------------------------
---        Test Controller Components Declaration         --
+--        Test Controller DUTonents Declaration         --
 --vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv--
 
   component test_ctrl_e is
     port (
-      rst  :  in  std_logic;
-  
+      rst            :  in  std_logic;
       --Transaction interface
-      
       stream_tx_rec  :  inout StreamRecType;
       stream_rx_rec  :  inout StreamRecType;
       sync_tx_rec    :  inout StreamRecType;
       sync_rx_rec    :  inout StreamRecType;
       pulse_rx_rec   :  inout StreamRecType;
       pulse_tx_rec   :  inout StreamRecType
-  
     );
   end component; 
 
   begin
-
     --dbg <= <<signal .TLM_tb.TestCtrl_1.dbg : std_logic>>;
-
 -----------------------------------------------------------
 --            OSVVM Clock and Reset Creation             --
 --vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv--
@@ -140,42 +138,45 @@ architecture testbench of TLM_tb is
     ) ;
 
 -----------------------------------------------------------
---             TLM Verification Components               --
+--             TLM Verification DUTonents               --
 --vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv--
 
-    ------------------------------------------
-    -- Graycode VC                          --
-    ------------------------------------------
+  ------------------------------------------
+  --      Graycode Transmitter VC         --
+  ------------------------------------------
     TX_GrayCode_VC : entity work.graycode_tx_vc
-    GENERIC MAP(
-            DATA_WIDTH => DATA_WIDTH
-        )
-    port map (
-      clk              => clk,
-      rst              => rst,
-      tx_valid         => pam_map_valid,
-      tx_last          => open,
-      tx_data          => pam_map_data,
-      --Transactio interface
-      trans_rec        => stream_tx_rec
-    );
+      generic map (
+        DATA_WIDTH  =>  DATA_WIDTH
+      )
+      port map (
+        clk         =>  clk,
+        rst         =>  rst,
+        tx_valid    =>  pam_map_valid,
+        tx_last     =>  open,
+        tx_data     =>  pam_map_data,
+        --Transactio interface
+        trans_rec   =>  stream_tx_rec
+      );
 
+  ------------------------------------------
+  --        Graycode Receiver VC          --
+  ------------------------------------------
     RX_GrayCode_VC : entity work.graycode_rx_vc
-    GENERIC MAP(
-            DATA_WIDTH => DATA_WIDTH
-        )
-    port map (
-      clk              => clk,
-      rst              => rst,
-      rx_write         => fifo_write,
-      rx_data          => fifo_data_in,
-      --Transactio interface
-      trans_rec        => stream_rx_rec
-    );
+      generic map (
+        DATA_WIDTH  =>  DATA_WIDTH
+      )
+      port map (
+        clk         =>  clk,
+        rst         =>  rst,
+        rx_write    =>  clk_sync_write,
+        rx_data     =>  clk_sync_data_in,
+        --Transactio interface
+        trans_rec   =>  stream_rx_rec
+      );
 
-    ------------------------------------------
-    -- Clk_sync VC                          --
-    ------------------------------------------
+  ------------------------------------------
+  --             Clk_sync VC              --
+  ------------------------------------------
     Clk_Sync_VC : entity work.clk_sync_rx_vc
     GENERIC MAP(
             DATA_WIDTH => DATA_WIDTH
@@ -185,37 +186,37 @@ architecture testbench of TLM_tb is
       clk_wr            =>  clk,
       rst               =>  rst,
 
-      rx_dat_in         =>  fifo_data_in,
-      rx_rd_in          =>  fifo_read,
-      rx_wr_in          =>  fifo_write,
+      rx_dat_in         =>  clk_sync_data_in,
+      rx_rd_in          =>  clk_sync_read,
+      rx_wr_in          =>  clk_sync_write,
       --
-      rx_dat_out	      =>  fifo_data_out,
-      rx_empty_out      =>  fifo_empty,
-      rx_full_out       =>  fifo_full,
+      rx_dat_out	      =>  clk_sync_data_out,
+      rx_empty_out      =>  clk_sync_empty,
+      rx_full_out       =>  clk_sync_full,
 
       --Transaction interface
       rx_trans_rec_in   =>  sync_tx_rec,
       rx_trans_rec_out  =>  sync_rx_rec
     );
 
-    ------------------------------------------
-    -- Pulse Shaper VC                      --
-    ------------------------------------------
+  ------------------------------------------
+  --           Pulse Shaper VC            --
+  ------------------------------------------
     Pulse_Shaper_VC : entity work.pulseshaper_rx_vc
-      GENERIC MAP(
-        DATA_WIDTH => DATA_WIDTH
+      generic map (
+        DATA_WIDTH    => DATA_WIDTH
       )
       port map (
-        clk             =>  clk_b,
-        rst             =>  rst,
-        rx_valid        =>  pulse_shaper_valid_out,
-        rx_data         =>  pulse_shaper_data_out,
-        rx_read         =>  fifo_read,
-        tx_empty        =>  pulse_shaper_valid_in,
-        tx_data         =>  fifo_data_out,
+        clk           =>  clk_b,
+        rst           =>  rst,
+        rx_valid      =>  pulse_shaper_valid_out,
+        rx_data       =>  pulse_shaper_data_out,
+        rx_read       =>  clk_sync_read,
+        tx_empty      =>  pulse_shaper_valid_in,
+        tx_data       =>  clk_sync_data_out,
         -- Transaction interfaces
-        rx_trans_rec    =>  pulse_rx_rec,
-        tx_trans_rec    =>  pulse_tx_rec
+        rx_trans_rec  =>  pulse_rx_rec,
+        tx_trans_rec  =>  pulse_tx_rec
       );
 
 -----------------------------------------------------------
@@ -223,9 +224,9 @@ architecture testbench of TLM_tb is
 --vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv--
 
   TestCtrl_1 : test_ctrl_e
-  GENERIC MAP(
-        DATA_WIDTH => DATA_WIDTH
-      )
+    generic map (
+      DATA_WIDTH     => DATA_WIDTH
+    )
     port map (
       rst            =>  rst,
       stream_tx_rec  =>  stream_tx_rec,
@@ -240,55 +241,67 @@ architecture testbench of TLM_tb is
 --                     TLM DUT's                         --
 --vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv--
 
-    GrayCode_COMP : entity work.pam_map
-    generic map (
-        DATA_WIDTH  =>  DATA_WIDTH
-    )
-    port map(
-        rst     =>  rst,
-        clk     =>  clk,
-        --
-        rx_dat  =>  pam_map_data(0),
-        rx_val  =>  pam_map_valid,
-        rx_full =>  fifo_full,
-        --
-        tx_dat  =>  fifo_data_in,
-        tx_wr   =>  fifo_write
-    );
-
-    Clk_Sync_COMP : ENTITY work.clk_sync
-        GENERIC MAP(
-            DATA_WIDTH  =>  DATA_WIDTH
-        )
-        PORT MAP(
-            clk_rd      =>  clk_b,
-            clk_wr      =>  clk,
-            --
-            rx_dat      =>  fifo_data_in,
-            rx_rd       =>  fifo_read,
-            rx_wr       =>  fifo_write,
-            --
-            tx_dat      =>  open,
-            tx_empty    =>  fifo_empty,
-            tx_full     =>  fifo_full
-        );
-
-    PulseShaper_COMP : entity work.pulse_shaper
+  ----------------------------------------------
+  --              Gray code DUT               --
+  ----------------------------------------------  
+    GrayCode_DUT : entity work.pam_map
       generic map (
         DATA_WIDTH  =>  DATA_WIDTH
       )
-      PORT MAP (
+      port map (
+        rst     =>  rst,
+        clk     =>  clk,
+        
+        rx_dat  =>  pam_map_data(0),
+        rx_val  =>  pam_map_valid,
+        rx_full =>  clk_sync_full,
+        
+        tx_dat  =>  clk_sync_data_in,
+        tx_wr   =>  clk_sync_write
+      );
+
+  ----------------------------------------------
+  --            Clock synchroniser            --
+  ----------------------------------------------  
+    Clk_Sync_DUT : ENTITY work.clk_sync
+      generic map (
+          DATA_WIDTH  =>  DATA_WIDTH
+      )
+      port map (
+        clk_rd        =>  clk_b,
+        clk_wr        =>  clk,
+      
+        rx_dat        =>  clk_sync_data_in,
+        rx_rd         =>  clk_sync_read,
+        rx_wr         =>  clk_sync_write,
+      
+        tx_dat        =>  open,
+        tx_empty      =>  clk_sync_empty,
+        tx_full       =>  clk_sync_full
+        );
+
+  ----------------------------------------------
+  --            Pulse Shaper DUT              --
+  ----------------------------------------------  
+    PulseShaper_DUT : entity work.pulse_shaper
+      generic map (
+        DATA_WIDTH  =>  DATA_WIDTH
+      )
+      port map (
         rst         =>  rst,
         clk         =>  clk_b,
         --
-        rx_dat      =>  fifo_data_out, 
+        rx_dat      =>  clk_sync_data_out, 
         rx_empty    =>  pulse_shaper_valid_in,
         --
         tx_dat      =>  pulse_shaper_data_out,
-        tx_read     =>  fifo_read,
+        tx_read     =>  clk_sync_read,
         tx_val      =>  pulse_shaper_valid_out
       );
 
+  ----------------------------------------------
+  --            Match Filter DUT              --
+  ----------------------------------------------  
     MatchFilter_DUT : entity work.match_filter
       generic map(
         DATA_WIDTH  =>  DATA_WIDTH
@@ -303,6 +316,9 @@ architecture testbench of TLM_tb is
         tx_val      => match_filter_valid_out
       );
 
+  ----------------------------------------------
+  --           Clock Recovery DUT             --
+  ----------------------------------------------  
     ClockRecovery_DUT : entity work.clk_recovery
       generic map (
         DATA_WIDTH  =>  DATA_WIDTH
@@ -318,22 +334,6 @@ architecture testbench of TLM_tb is
         tx_wr       => clk_recovery_wr_out,
         tx_val      => clk_recovery_valid_out 
       );
-   
--- ENTITY clk_recovery IS
--- GENERIC (
---     DATA_WIDTH : INTEGER);
--- PORT (
---     rst      : IN  STD_LOGIC;
---     clk      : IN  STD_LOGIC;
---     --
---     rx_dat   : IN  STD_LOGIC_VECTOR(27 DOWNTO 0);
---     rx_val   : IN  STD_LOGIC;
---     --
---     tx_dat   : OUT STD_LOGIC_VECTOR(27 DOWNTO 0);
---     tx_wr    : OUT STD_LOGIC;
---     tx_val   : OUT STD_LOGIC
--- );
--- END clk_recovery;
 
 -----------------------------------------------------------
 --                     TLM Top DUT                       --
