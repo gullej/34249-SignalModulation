8bc44f7722f3182097fb02035aacd0af13518ad2
gullej <gustav@juul-jorgensen.com>
2023-06-16 09:19:49 +0200

    Fixed downsampler

--- src/downsample.vhd
+++ src/downsample.vhd
@@ -1,6 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
 ENTITY downsample IS
     GENERIC (
@@ -28,11 +29,31 @@ SIGNAL rd_en_reg1, rd_en_reg2, rd_en_reg3 : STD_LOGIC;
 
 BEGIN
 
-tx_val <= rd_en_reg3 when RISING_EDGE(clk_rd);
+    writer : process(clk_wr)
+    begin
+        IF (RISING_EDGE(clk_wr)) THEN
+            wr_addr <= wr_addr;
+            rd_en_reg1 <= rx_rd;
+            rd_en_reg2 <= rd_en_reg1;
 
-rd_en_reg1 <= rx_rd when RISING_EDGE(clk_wr);
-rd_en_reg2 <= rd_en_reg1 when RISING_EDGE(clk_wr);
-rd_en_reg3 <= rd_en_reg2 when RISING_EDGE(clk_rd);
+            IF (rx_wr = '1') THEN
+                wr_addr <= wr_addr + 1;
+            END IF;
+
+            IF (rst = '1') THEN
+                wr_addr <= (others => '0');
+            END IF;
+        END IF;
+
+    end process writer;
+
+    reader : process(clk_rd)
+    begin
+        IF (RISING_EDGE(clk_rd)) THEN
+            tx_val <= rd_en_reg3;
+            rd_en_reg3 <= rd_en_reg2;
+        END IF;
+    end process reader;
 
     ram : entity work.dual_port_ram
     PORT MAP (
